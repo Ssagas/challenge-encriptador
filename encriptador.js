ad4726855038d1fd147a665c258e61030ec03db4
Sonia Sagastume <78199421+Ssagas@users.noreply.github.com>
2023-01-18 18:49:53 -0600

    creando repositorio

---
+++ encriptador.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function(){
+    let cajaT = document.getElementById("caja-texto");
+    let textoEncriptado = document.getElementById("texto-encriptado");
+    let btnEncriptar = document.getElementById('boton-encriptar');   
+});
+
+
+function encriptar(){
+    console.log("entro a encriptar")
+}
+
+
+
+
