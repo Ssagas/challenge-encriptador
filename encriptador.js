4c2b0a829da892bb3ce0b4139c18058899b9ae63
Sonia Sagastume <78199421+Ssagas@users.noreply.github.com>
2023-01-21 18:00:45 -0600

    agregando funcion encriptar

--- encriptador.js
+++ encriptador.js
@@ -1,20 +1,49 @@
+ //document.addEventListener("DOMContentLoaded", function() {
+    
+    let tEncriptado = "";
 
-    var cajaT = document.getElementById("caja-texto");
-    var textoEncriptado = document.getElementById("texto-encriptado");
-    var btnEncriptar = document.getElementById('boton-encriptar');   
-    var mensaje = document.getElementById ("msg");
+    let reglasEncriptador = {  //declarando el objeto para realizar la encriptación
+        "a" : "ai",
+        'e': 'enter',
+        'i': 'imes',
+        'o': 'ober',
+        'u': 'ufat',
+    };
+
+    let propiedades = Object.keys(reglasEncriptador);
+    
+    function btnEncriptar(){
+
+        let cajaT = document.getElementById("caja-texto");
+        let textoEncriptado = document.getElementById("texto-encriptado");
    
-    var btnCopiar = document.getElementById ("boton-copiar");
+        let mensaje = document.getElementById ("msg");
+        let img = document.getElementById ('imagen');   
+        let btnCopiar = document.getElementById ("boton-copiar");
 
-
-
-function encriptar(){
-    let imagen = document.getElementById ("imagen");
-    imagen.style.display ="none";
-    mensaje.style.display = "none";
-    btnCopiar.style.display = "block";
-    console.log("funciona el botón");
-}
+        textoEncriptado.style.display = "block";
+        if(img.style.display=="block") img.style.display = "none";
+        mensaje.style.display = "none";
+        btnCopiar.style.display = "block";
+        
+        
+        textoEncriptado.value= encriptarTexto(cajaT.value, propiedades.length);
+        
+    }
+    
+    function encriptarTexto(texto,i){
+        if((i<=propiedades.length)&&(i!=0)){
+            tEncriptado = texto.replaceAll(propiedades[i-1], reglasEncriptador[propiedades[i-1]]);
+            i--;
+            if (i>=0) encriptarTexto(tEncriptado, i);
+        } 
+           return tEncriptado;
+    }  //end de la funcion encriptarTexto
+  //});
+    
+  
+    
+     
 
 
 
